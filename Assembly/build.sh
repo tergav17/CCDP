7577085274797068dd2986603dd233b45a36c691
tergav17 <gctersteeg@gmail.com>
2022-05-24 18:52:56 -0500

    Assembly sound checkin

---
+++ Assembly/build.sh
@@ -0,0 +1,35 @@
+aswrx6 -l listing_ccdp.txt ccdp.asm
+ldwrx6 -b -C 28672 ccdp.o -o ccdp.raw
+dd if=ccdp.raw bs=28672 skip=1 of=bin/CCDP.BIN
+cat headers/32K.header bin/CCDP.BIN > bin/CCDP.SYS
+rm ccdp.o ccdp.raw bin/CCDP.BIN
+
+aswrx6 -l listing_hello.txt hello.asm
+ldwrx6 -b -C 512 hello.o -o hello.raw
+dd if=hello.raw bs=512 skip=1 of=bin/HELLO.BIN
+rm hello.raw hello.o
+
+aswrx6 -l listing_dir.txt dir.asm
+ldwrx6 -b -C 512 dir.o -o dir.raw
+dd if=dir.raw bs=512 skip=1 of=bin/DIR.BIN
+rm dir.raw dir.o
+
+aswrx6 -l listing_type.txt type.asm
+ldwrx6 -b -C 512 type.o -o type.raw
+dd if=type.raw bs=512 skip=1 of=bin/TYPE.BIN
+rm type.raw type.o
+
+aswrx6 -l listing_copy.txt copy.asm
+ldwrx6 -b -C 512 copy.o -o copy.raw
+dd if=copy.raw bs=512 skip=1 of=bin/COPY.BIN
+rm copy.raw copy.o
+
+aswrx6 -l listing_boot.txt boot.asm
+ldwrx6 -b -C 512 boot.o -o boot.raw
+dd if=boot.raw bs=512 skip=1 of=bin/BOOT.BIN
+rm boot.raw boot.o
+
+aswrx6 -l listing_bootstrap.txt bootstrap.asm
+rm bootstrap.o
+
+mv *.txt listings/
